6c28ffa8005abc6221b9b325f2074339f1e70214
EBuravskiy <elric0sphinx@gmail.com>
2025-12-11 16:59:53 +0500

    A corresponding object has been developed for the task file.

---
+++ AppDocumentManagement.DB/Entities/ProductionTaskFile.cs
@@ -0,0 +1,12 @@
+﻿namespace AppDocumentManagement.DB.Entities
+{
+    public class ProductionTaskFile
+    {
+        public int ProductionTaskFileID { get; set; }
+        public string ProductionTaskFileName { get; set; }
+        public string ProductionTaskFileExtension { get; set; }
+        public byte[] ProductionTaskFileData { get; set; }
+        public ProductionTask ProductionTask { get; set; }
+        public int ProductionTaskID { get; set; }
+    }
+}
